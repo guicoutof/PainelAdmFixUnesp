598338b428ec344fe86f88e9d20bd913d2e342d3
Leonardo Yudi <lol_leonardohiguti@outlook.com>
2019-07-14 19:02:12 -0300

    teste

--- paineladmfixunesp/src/App.jsx
+++ paineladmfixunesp/src/App.jsx
@@ -6,13 +6,13 @@ import Menu from './componentes/Menu'
 import Header from './componentes/Header'
 import Footer from './componentes/Footer'
 
-export default function App(){
+export default function App() {
     return <div className="app">
-                <Header />
-                <div className="conteudo">
-                    <Menu />
-                    <Painel />
-                </div>
-                <Footer />
-            </div>
+        <Header />
+        <div className="conteudo">
+            <Menu />
+            <Painel />
+        </div>
+        <Footer />
+    </div>
 }
