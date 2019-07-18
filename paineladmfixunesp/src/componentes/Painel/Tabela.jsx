dff5ed3bdf6ec62ef7a7c72ef9525c99df264545
Guilherme Couto <guilherme.couto@unesp.br>
2019-07-18 09:39:49 -0300

    Menu & Header
    correções

--- paineladmfixunesp/src/componentes/Painel/Tabela.jsx
+++ paineladmfixunesp/src/componentes/Painel/Tabela.jsx
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
 import './Tabela.css'
 
-export default () => 
-<div>
-  <table className="tabela">
-    <thead className="cabecalhoTabela">
-      <tr>
-          <Grid container spacing={2}>
-          <Grid item xs={2}><div>Email</div></Grid>
-          <Grid item xs={3}><div>Descrição</div></Grid>
-          <Grid item xs={1}><div>Bloco</div></Grid>
-          <Grid item xs={1}><div>Piso</div></Grid>
-          <Grid item xs={1}><div>Imagem</div></Grid>
-          <Grid item xs={2}><div>Assunto</div></Grid>
-          <Grid item xs={1}><div>Resolvido</div></Grid>
-          <Grid item xs={1}><div>Agrupar</div></Grid>
-          </Grid>
-        </tr>
-    </thead>
-    <tbody>
+export default class Tabela extends Component {
+    render(){
+        return(
+          <div>
+            <table className="tabela">
+              <thead className="cabecalhoTabela">
+                <tr>
+                    <Grid container spacing={2}>
+                    <Grid item xs={2}><div>Email</div></Grid>
+                    <Grid item xs={3}><div>Descrição</div></Grid>
+                    <Grid item xs={1}><div>Bloco</div></Grid>
+                    <Grid item xs={1}><div>Piso</div></Grid>
+                    <Grid item xs={1}><div>Imagem</div></Grid>
+                    <Grid item xs={2}><div>Assunto</div></Grid>
+                    <Grid item xs={1}><div>Resolvido</div></Grid>
+                    <Grid item xs={1}><div>Agrupar</div></Grid>
+                    </Grid>
+                  </tr>
+              </thead>
+              <tbody>
 
-    </tbody>
-  </table>
-</div>
+              </tbody>
+            </table>
+          </div>
+        )
+    }
+} 
