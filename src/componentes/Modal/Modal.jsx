fd13ea7b29453532ea702f461836710a8bb77745
Guilherme Couto <gui_coutof@hotmail.com>
2019-07-26 19:46:47 -0300

    Correção no repositorio
    Alteração na hierarquia de pastas onde os arquivos subiram uma pasta

--- src/componentes/Modal/Modal.jsx
+++ src/componentes/Modal/Modal.jsx
@@ -48,3 +48,30 @@ export function ModalSemInput(props){
                 </div>
             </Modal>
 }
+
+export function ModalHelp(props){
+
+    return  <Modal 
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+                open={props.open}
+				onClose={props.handleClose}
+                >
+                <div className="modalHelp">
+                    <h1 className="tituloModal">Help</h1>
+					<div className="text">
+                        <br></br>
+                        <p>É necessario definir um assunto para que os assuntos em comum e que estão no mesmo local possam ser combinados.</p>
+                        <br></br>
+                        <p>É possivel criar assuntos clicando no botão "Criar Assunto" e após sua criação é permitido editar e remover um assunto.</p>
+                        <br></br>
+                        <p>Não é permitido editar um assunto que estão sendo usado em algum problema pendente.</p>
+                        <br></br>
+                        <p>Ao confirmar uma resolução de problemas, um e-mail será enviado aos responsáveis pela solicitação.</p>
+                        <br></br>
+					</div>
+                   <Button className="fechar" variant="contained" color="secondary"
+                        onClick={props.handleClose}>Fechar</Button>
+                </div>
+            </Modal>
+}
