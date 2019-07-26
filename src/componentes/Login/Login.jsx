b02b521d451a609638b15168dcd9d8041a97c446
Guilherme Couto <gui_coutof@hotmail.com>
2019-07-26 20:06:32 -0300

    Correções Link
    Acréscimo de <BrowserRouter> para o <Link> funcionar no 5.0

--- src/componentes/Login/Login.jsx
+++ src/componentes/Login/Login.jsx
@@ -1,52 +1,54 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { BrowserRouter, Link } from 'react-router-dom'
 import './Login.css'
 
 export default function Login() {
   return (
-    <div class="limiter">
-      <div class="container-login100">
-        <div class="wrap-login100 p-l-55 p-r-55 p-t-60 p-b-30">
-          <form class="login100-form validate-form">
-            <span class="login100-form-title p-b-33">
+    <div id="login" className="limiter">
+      <div className="container-login100">
+        <div className="wrap-login100 p-l-55 p-r-55 p-t-60 p-b-30">
+          <form className="login100-form validate-form">
+            <span className="login100-form-title p-b-33">
               Login
 					  </span>
 
-            <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
-              <input class="input100" type="text" name="email" placeholder="Email" />
-              <span class="focus-input100-1"></span>
-              <span class="focus-input100-2"></span>
+            <div className="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+              <input className="input100" type="text" name="email" placeholder="Email" />
+              <span className="focus-input100-1"></span>
+              <span className="focus-input100-2"></span>
 
             </div>
 
-            <div class="wrap-input100 rs1 validate-input" data-validate="Password is required">
-              <input class="input100" type="password" name="pass" placeholder="Senha" />
-              <span class="focus-input100-1"></span>
-              <span class="focus-input100-2"></span>
+            <div className="wrap-input100 rs1 validate-input" data-validate="Password is required">
+              <input className="input100" type="password" name="pass" placeholder="Senha" />
+              <span className="focus-input100-1"></span>
+              <span className="focus-input100-2"></span>
             </div>
 
-            <div class="container-login100-form-btn m-t-20">
-              <button class="login100-form-btn">
-              <Link to="/home">Confirmar</Link>
+            <div className="container-login100-form-btn m-t-20">
+              <button className="login100-form-btn">
+              <BrowserRouter>
+                <Link to="/home">Confirmar</Link>
+              </BrowserRouter>
               </button>
             </div>
 
-            <div class="text-center p-t-45 p-b-4">
-              <span class="txt1">
+            <div className="text-center p-t-45 p-b-4">
+              <span className="txt1">
                 Esqueceu &nbsp;
 						      </span>
 
-              <a href="#" class="txt2 hov1">
+              <a href="#login" className="txt2 hov1">
                 Email / Senha ?
 						      </a>
             </div>
 
-            <div class="text-center">
-              <span class="txt1">
+            <div className="text-center">
+              <span className="txt1">
                 Ainda não tem conta? &nbsp;
 						      </span>
 
-              <a href="#" class="txt2 hov1">
+              <a href="#login" className="txt2 hov1">
                 Cadastre-se
 					    	</a>
             </div>
