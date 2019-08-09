f663eb3d9b5bfc4c2084ad988512f310934b22ad
Guilherme Couto <guilherme.couto@unesp.br>
2019-08-09 16:05:51 -0300

    Modal Imagem
    Construção de um modal para exibir a imagem de um problema.

--- src/componentes/Painel/Tabela.jsx
+++ src/componentes/Painel/Tabela.jsx
@@ -11,6 +11,8 @@ import TableRow from '@material-ui/core/TableRow';
 import Imagem from '@material-ui/icons/Image'
 import DoneIcon from '@material-ui/icons/Done'
 import Fab from '@material-ui/core/Fab';
+import IconButton from '@material-ui/core/IconButton';
+import { ModalImage } from '../Modal/Modal'
 
 import './Tabela.css'
 
@@ -21,6 +23,27 @@ export default function Tabela(props) {
   const [values, setValues] = React.useState({
     assunto: []
   });
+
+  const [linha, setLinha] = React.useState({
+    id:0,
+    email:'',
+    descricao:'',
+    bloco:'',
+    piso:'',
+    imagem:'',
+    grupo:0
+  })
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = (row) => {
+    setOpen(true);
+    setLinha(row);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   function handleChange(event) {
     setValues(oldValues => ({
@@ -55,7 +78,17 @@ export default function Tabela(props) {
               </TableCell>
               <TableCell align="center">{row.bloco}</TableCell>
               <TableCell align="center">{row.piso}</TableCell>
-              <TableCell align="center"><Imagem/></TableCell>
+              <TableCell align="center">
+                <IconButton size="small" aria-label="Imagem"
+                    onClick={()=>handleOpen(row)}>
+                    <Imagem/>
+                  </IconButton>
+              </TableCell>
+                <ModalImage className="imagem"
+                  open={open}
+                  handleClose={handleClose}
+                  conteudo={linha.imagem}
+                ></ModalImage>
               <TableCell align="center"><Select value={values.assunto}
                                           onChange={handleChange}
                                           inputProps={{
